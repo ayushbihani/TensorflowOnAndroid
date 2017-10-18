d948f40243e049ac2a065545b4eb42a8b2c69d5d
aysuhbihani <bihani37@gmail.com>
2017-10-18 19:52:26 +0530

    updated

--- TextHelpers.py
+++ TextHelpers.py
@@ -1,10 +1,11 @@
-import numpy as np 
 import itertools
+import re
+import numpy as np
 from collections import Counter
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-
+from nltk.corpus import words
 '''
 Since labels are in text, we would want to convert them into a numeric format.
 DESC : Description
@@ -29,22 +30,99 @@ def readData():
     f.close()
     return [questions, labels]            
 
-def remove_stopwords(data):
-    
+def remove_stopwords(X):   
     """
     Removes unnecessary words from dataset
     """
-
     lemmatizer = WordNetLemmatizer()
     questions = []
-    stop_words = set(stopwords.words('english'))
-    for i in data:
-        word_tokens = word_tokenize(i)
-        filtered_sentence = [lemmatizer.lemmatize(w) for w in word_tokens if not w in stop_words]
-        questions.append(' '.join(filtered_sentence))
-
-    print(questions)
+    stopWords = set(stopwords.words('english'))
+    for question in X:
+        wordTokens = word_tokenize(question)
+        filteredSentence = [lemmatizer.lemmatize(w) for w in wordTokens if not w in stopWords]
+        questions.append(' '.join(filteredSentence))
     return questions
 
-X,y = readData()   
-remove_stopwords(X)
+def cleanInput(string):
+    """
+	removing whitespaces,useless characters from dataset.
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", "", string)
+    string = re.sub(r"\'ve", "", string)
+    string = re.sub(r"n\'t", "", string)
+    string = re.sub(r"\'re", "", string)
+    string = re.sub(r"\'d", "", string)
+    string = re.sub(r"\'ll", "", string)
+    string = re.sub(r",", "", string)
+    string = re.sub(r"!", "", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+def createVocabulary(X):
+    
+    '''
+        Creates a vocabulary of words and maps them to an index.
+    '''
+    word2index = {}
+    vocabularySet = Counter()
+    vocabularyFile = open("vocabulary.txt","w")
+    english_words = set(words.words())
+    for questions in X:
+        questionsWords = questions.split(' ')
+        for word in questionsWords:
+            word = cleanInput(word.lower())
+            if(word in english_words):
+                vocabularySet[word] = vocabularySet[word]+1
+
+    for index,word in enumerate(vocabularySet):
+        word2index[word] = index
+        vocabularyFile.write(word + "\n")
+        
+    return [word2index, vocabularySet, vocabularyFile]   
+
+def featureExtractionFrequency(X, vocabularySet, word2index):
+    '''
+        Creates word vectors using frequency based representation
+    '''
+
+    if(len(word2index)!= len(vocabularySet)):
+        print("Error!")
+        return -1
+    lengthOfVector = len(vocabularySet)    
+    featureDataset = []
+    for question in X:
+        newFeatureVector = np.zeros(lengthOfVector, dtype = float)
+
+        for word in question:
+            word = re.sub(r"\b[a-zA-Z]\b", "",word)
+            if(word.lower() in word2index.keys()):
+                newFeatureVector[word2index[word.lower()]]+=1
+        featureDataset.append(newFeatureVector)
+    return [np.array(featureDataset), lengthOfVector]    
+
+def featureExtactionUsingIndex(X, word2index, vocabularySet):
+  if(len(word2index)!=len(vocabularySet)):
+    return -1
+  lengthOfVector = len(vocabularySet)
+  maxLength = max([len(sentence.split(' ')) for sentence in X])
+  featureDataset = []
+  for question in X:
+    increment = 0
+    newFeatureVector = np.zeros(lengthOfVector, dtype = float)
+    for word in question:
+      if(word in word2index.keys()):
+        newFeatureVector[increment] = word2index[word]
+        increment+=1
+    featureDataset(newFeatureVector)
+  return [np.array(featureDataset), lengthOfVector]
+
+def callFunctions():
+  X,Y = readData()   
+  X = remove_stopwords(X) 
+  word2index, vocabularySet, vocabularyFile = createVocabulary(X)
+  X, lengthOfVector = featureExtractionFrequency(X, vocabularySet, word2index)
+  print(X.shape)
+  return [X,Y,lengthOfVector]
+
+callFunctions()
